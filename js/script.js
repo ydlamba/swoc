cd37b39d55d2df5c3d9e1300659b2c24242b0bd6
Yash Dev Lamba <yashdev18@gmail.com>
2016-12-14 00:56:57 +0530

    front-end work started

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var slideSpeed = 2000;
+
+$(".nav_r").click(function(){
+
+	$(".horizontal").animate({"margin-left":"-100vw"},slideSpeed);	
+
+
+});
+
+
+
+$(".nav_d").click(function(){
+
+	$(".horizontal").animate({"margin-left":"-100vw"},slideSpeed);	
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+});
