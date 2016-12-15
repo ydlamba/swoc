c136aa45f77c3a21ba2d4dc88da02d32de7b7cb2
Yash Dev Lamba <yashdev18@gmail.com>
2016-12-15 23:04:12 +0530

    starting animation added and navigation updated

--- js/script.js
+++ js/script.js
@@ -20,54 +20,101 @@ $(document).ready(function(){
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });*/
 
+	
 
 
-
-	var slideSpeed = 2000;
+	var slideSpeed = 1000;
 	var position = 5; /*position by numkeypad '5 means center'*/ 
 	var $horizontal = $(".horizontal");
 	var $vertical = $(".vertical");
+	var $center = $("#center");
+	var $c_main = $("#center>main")
 	var $nav_r = $(".nav_r");
 	var $nav_l = $(".nav_l");
 	var $nav_u = $(".nav_u");
 	var $nav_d = $(".nav_d");
 	var $nav_home = $(".nav_home");
 
-$nav_r.click(function openRight(){
-
-	$horizontal.animate({"margin-left":"-100vw"},slideSpeed);
-    $vertical.animate({"margin-right":"200vw"},slideSpeed);
-    position = 6;
-
-});
-$nav_l.click(function openLeft(){
-
-	$horizontal.animate({"margin-left":"100vw"},slideSpeed);
-    $vertical.animate({"margin-left":"200vw"},slideSpeed);
-    position = 4;
-
-});
 
 
-
-$nav_d.click(function openDown(){
-
-	$vertical.animate({"margin-top":"-700px"},slideSpeed);	
-	position = 2;
-
-});
+setTimeout(function(){
+	$center.css("background-image","url(./img/background3.jpg)");
+	setTimeout(function(){
+		$c_main.css("transform","scale(100)");
+		$c_main.show(500);
+		$c_main.css("transform","scale(1)");
+	},1500);
+},2000);
 
 
 
 
-$nav_u.click(function openUp(){
 
-	$vertical.animate({"margin-top":"700px"},slideSpeed);
-	position = 8;	
 
-});
 
-$nav_home.click(function(){
+function goRight(){
+
+
+	if(position === 5){
+		$horizontal.animate({"margin-left":"-100vw"},slideSpeed);
+	    $vertical.animate({"margin-right":"200vw"},slideSpeed);
+	    position = 6;	
+	}else{
+		backToHome();
+		goRight();
+    	position = 6;
+    }
+
+}
+
+function goLeft(){
+
+
+	if(position === 5){
+		$horizontal.animate({"margin-left":"100vw"},slideSpeed);
+	    $vertical.animate({"margin-left":"200vw"},slideSpeed);
+	    position = 4;
+	}else{
+		backToHome();
+		goLeft();
+    	position = 4;
+    }
+
+}
+
+
+
+function goDown(){
+
+	if(position === 5){
+		$vertical.animate({"margin-top":"-700px"},slideSpeed);	
+		position = 2;	
+	}else{
+		backToHome();
+		goDown();
+    	position = 2;
+    }
+
+
+}
+
+
+
+
+function goUp(){
+
+	if(position === 5){
+		$vertical.animate({"margin-top":"700px"},slideSpeed);
+		position = 8;	
+	}else{
+		backToHome();
+		goUp();
+    	position = 8;
+    }
+
+}
+
+function backToHome(){
 
 	if(position === 2){
 		$vertical.animate({"margin-top":"0"},slideSpeed);
@@ -85,7 +132,13 @@ $nav_home.click(function(){
 	    position = 5;		
 	}
 
-});
+}
+
+$nav_home.click(backToHome);
+$nav_r.click(goRight);
+$nav_l.click(goLeft);
+$nav_u.click(goUp);
+$nav_d.click(goDown);
 
 
 
